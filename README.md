254976051f4e17df4d93d3d128827c8e5f023d21
Nick Mosher <nick@mosher.me>
2019-08-18 20:15:10 -0500

    migrated from dreamhost

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # nick.mosher.me
+
+Based on: 
+
+=============================
+
+The Personal Page
+Version 1.0
+January 19, 2010
+Created by Naz Hamid
+Original: www.nazhamid.com
+License: MIT/GPL
+
+https://github.com/weightshift/The-Personal-Page
+http://weightshift.com/memo/the-personal-page
+
+The site makes use of the jQuery Backstretch plugin 
+from Scott Robbin. Gracias.
+http://srobbin.com/blog/jquery-plugins/jquery-backstretch/
+
+=============================
+
+This simple one-page website is a way for people to have a very quick and easy personable website that aggregates your activity and positions a simple logo, a portrait and some description text in a nicely-formatted manner. This is licensed under the MIT and GPL licenses. 
+
+See the original: http://nazhamid.com
+
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include 'head.html'; ?>
+<?php include 'top.html'; ?>
+<div id="about">
+	<p>
+		I am <a href="http://www.linkedin.com/in/mosherdesign" title="My linkedin profile">Nick Mosher</a> and I am an 
+		<a href="http://en.wikipedia.org/wiki/Interaction_design" title="a web definition of Interaction Design">Interaction
+		</a> and <a href="http://en.wikipedia.org/wiki/Experience_design" title="a web definition of Experience Design">
+		User Experience Designer</a>. I create web sites, intranet 
+		platforms, and web applications always with the customer's intuitive and effective use at the forefront of my planning.
+	</p>
+	<p>
+		Currently, I am a User Experience Designer for the <a href="http://www.aafp.org/" 
+		title="AAFP website">American Academy of Family Physicians</a>. At the AAFP, I work to champion the user experience 
+        on websites such as <a href="http://www.aafp.org/" 
+		title="AAFP website">AAFP.org</a>.
+	</p>
+	<p>
+		Please read all about my brilliant wife, <a href="http://emily.mosher.me" 
+		title="writer, dancer, wonderful wife">Emily</a> next. </p>
+</div>
+<?php include 'footer.html'; ?>
