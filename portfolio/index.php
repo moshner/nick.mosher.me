60b82afd1698001e81389756cc3fac53cabf9f0b
Nick Mosher <nick@mosher.me>
2019-08-18 20:27:14 -0500

    migrated more to flat html pages

--- portfolio/index.php
+++
@@ -1,7 +0,0 @@
-<?php include 'head.html'; ?>
-<?php include '../top.html'; ?>
-<div id="about" class="portfolio-intro">
-	<h1>My UX Portfolio</h1>
-    <p>Below you will find selected portions of my work as a User Experience Designer.</p>
-</div>
-<?php include 'footer.html'; ?>
